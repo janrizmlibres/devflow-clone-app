cf8ca0e6ef923a16c4aad54ce846ae3e03337fef
Janriz Libres <libres.janriz@gmail.com>
2025-06-04 12:30:31 +0800

    finalize eslint and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
-/* eslint-disable camelcase */
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono as GeistMono } from "next/font/google";
 import React from "react";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
