2dc9ce1da9c536f0675156c0f78e2588cc226570
Janriz Libres <libres.janriz@gmail.com>
2025-06-04 14:43:09 +0800

    configure tailwind theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-black text-violet-700">Welcome World!</h1>
-    </div>
+    <>
+      <div className="m-6 rounded-md border-4 border-violet-500 p-4">
+        <h1 className="text-3xl font-black text-violet-700">Welcome World!</h1>
+      </div>
+      <div className="m-6 rounded-md border-4 border-black p-4">
+        <h1 className="font-space-grotesk text-3xl font-black">
+          Welcome World!
+        </h1>
+      </div>
+    </>
   );
 };
 
