01250af0154eca32c33fae953ca8a8e466ccae30
Janriz Libres <libres.janriz@gmail.com>
2025-06-09 15:50:25 +0800

    implement find user by email

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -12,13 +12,13 @@ export async function GET(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  if (!id) throw new NotFoundError("User not found");
+  if (!id) throw new NotFoundError("User");
 
   try {
     await dbConnect();
 
     const user = await User.findById(id);
-    if (!user) throw new NotFoundError("User not found");
+    if (!user) throw new NotFoundError("User");
 
     return NextResponse.json({ success: true, data: user }, { status: 200 });
   } catch (error) {
@@ -31,13 +31,13 @@ export async function DELETE(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  if (!id) throw new NotFoundError("User not found");
+  if (!id) throw new NotFoundError("User");
 
   try {
     await dbConnect();
 
     const user = await User.findByIdAndDelete(id);
-    if (!user) throw new NotFoundError("User not found");
+    if (!user) throw new NotFoundError("User");
 
     return NextResponse.json({ success: true, data: user }, { status: 200 });
   } catch (error) {
@@ -50,7 +50,7 @@ export async function PUT(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  if (!id) throw new NotFoundError("User not found");
+  if (!id) throw new NotFoundError("User");
 
   try {
     await dbConnect();
@@ -62,7 +62,7 @@ export async function PUT(
       new: true,
     });
 
-    if (!updatedUser) throw new NotFoundError("User not found");
+    if (!updatedUser) throw new NotFoundError("User");
 
     return NextResponse.json(
       { success: true, data: updatedUser },
