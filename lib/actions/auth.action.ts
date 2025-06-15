2837143767457fe9913d23f59a95639141b55924
Janriz Libres <libres.janriz@gmail.com>
2025-06-15 19:59:39 +0800

    implement votes server actions

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -6,7 +6,6 @@ import mongoose, { HydratedDocument } from "mongoose";
 import { signIn } from "@/auth";
 import Account from "@/database/account.model";
 import User, { IUser } from "@/database/user.model";
-import { ActionResponse, ErrorResponse } from "@/types/global";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
--- lib/validations.ts
+++ lib/validations.ts
@@ -175,3 +175,15 @@ export const AIAnswerSchema = z.object({
     .min(100, { message: "Answer has to be more than 100 characters." }),
   userAnswer: z.string().optional(),
 });
+
+export const CreateVoteSchema = z.object({
+  targetId: z.string().min(1, { message: "Target ID is required." }),
+  targetType: z.enum(["Question", "Answer"], {
+    message: "Invalid target type.",
+  }),
+  voteType: z.enum(["upvote", "downvote"], { message: "Invalid vote type." }),
+});
+
+export const UpdateVoteCountSchema = CreateVoteSchema.extend({
+  change: z.number().int().min(-1).max(1),
+});
