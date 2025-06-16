e53e55e4fedcd125bb4ddff6df16029e81601fe2
Janriz Libres <libres.janriz@gmail.com>
2025-06-16 10:57:16 +0800

    add save to collection action

---
+++ lib/actions/collection.action.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import ROUTES from "@/constants/routes";
+import { Collection, Question } from "@/database";
+
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { NotFoundError } from "../http-errors";
+import { CollectionBaseSchema } from "../validations";
+
+export async function toggleSaveQuestion(
+  params: CollectionBaseParams
+): Promise<ActionResponse<{ saved: boolean }>> {
+  const validationResult = await action({
+    params,
+    schema: CollectionBaseSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = params;
+  const userId = validationResult.session?.user?.id;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) throw new NotFoundError("Question");
+
+    const collection = await Collection.findOne({
+      author: userId,
+      question: questionId,
+    });
+
+    if (collection) {
+      await Collection.findByIdAndDelete(collection._id);
+
+      return { success: true, data: { saved: false } };
+    }
+
+    await Collection.create({ author: userId, question: questionId });
+
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return { success: true, data: { saved: true } };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
