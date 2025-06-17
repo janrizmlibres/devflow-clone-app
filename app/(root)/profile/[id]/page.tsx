1a3dc50b282a82bbb9d038e293a292016cf179e9
Janriz Libres <libres.janriz@gmail.com>
2025-06-17 16:28:19 +0800

    develop profile ui with stats

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -5,6 +5,7 @@ import { notFound } from "next/navigation";
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ProfileLink from "@/components/user/ProfileLink";
+import Stats from "@/components/user/Stats";
 import UserAvatar from "@/components/UserAvatar";
 import { getUser } from "@/lib/actions/user.action";
 import { RouteParams } from "@/types/module";
@@ -28,55 +29,65 @@ const Profile = async ({ params }: RouteParams) => {
     user;
 
   return (
-    <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
-      <div className="flex flex-col items-start gap-4 lg:flex-row">
-        <UserAvatar
-          id={_id}
-          name={name}
-          imageUrl={image}
-          className="size-[140px] rounded-full object-cover"
-          fallbackClassName="text-6xl"
-        />
+    <>
+      <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <UserAvatar
+            id={_id}
+            name={name}
+            imageUrl={image}
+            className="size-[140px] rounded-full object-cover"
+            fallbackClassName="text-6xl"
+          />
 
-        <div className="mt-3">
-          <h2 className="h2-bold text-dark100_light900">{name}</h2>
-          <p className="paragraph-regular text-dark200_light800">@{username}</p>
-
-          <div className="mt-5 flex-start flex-wrap gap-5">
-            {portfolio && (
-              <ProfileLink
-                imgUrl="/icons/link.svg"
-                href={portfolio}
-                title="Portfolio"
-              />
-            )}
-            {location && (
-              <ProfileLink imgUrl="/icons/location.svg" title="Portfolio" />
-            )}
-            <ProfileLink
-              imgUrl="/icons/calendar.svg"
-              title={format(createdAt, "MMMM yyyy")}
-            />
-          </div>
-
-          {bio && (
-            <p className="mt-8 paragraph-regular text-dark400_light800">
-              {bio}
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{name}</h2>
+            <p className="paragraph-regular text-dark200_light800">
+              @{username}
             </p>
+
+            <div className="mt-5 flex-start flex-wrap gap-5">
+              {portfolio && (
+                <ProfileLink
+                  imgUrl="/icons/link.svg"
+                  href={portfolio}
+                  title="Portfolio"
+                />
+              )}
+              {location && (
+                <ProfileLink imgUrl="/icons/location.svg" title="Portfolio" />
+              )}
+              <ProfileLink
+                imgUrl="/icons/calendar.svg"
+                title={format(createdAt, "MMMM yyyy")}
+              />
+            </div>
+
+            {bio && (
+              <p className="mt-8 paragraph-regular text-dark400_light800">
+                {bio}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          {loggedInUser?.user?.id === id && (
+            <Link href="/profile/edit">
+              <Button className="min-h-12 min-w-44 btn-secondary px-4 py-3 paragraph-medium text-dark300_light900">
+                Edit Profile
+              </Button>
+            </Link>
           )}
         </div>
-      </div>
+      </section>
 
-      <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
-        {loggedInUser?.user?.id === id && (
-          <Link href="/profile/edit">
-            <Button className="min-h-12 min-w-44 btn-secondary px-4 py-3 paragraph-medium text-dark300_light900">
-              Edit Profile
-            </Button>
-          </Link>
-        )}
-      </div>
-    </section>
+      <Stats
+        totalQuestions={totalQuestions}
+        totalAnswers={totalAnswers}
+        badges={{ GOLD: 0, SILVER: 0, BRONZE: 0 }}
+      />
+    </>
   );
 };
 
