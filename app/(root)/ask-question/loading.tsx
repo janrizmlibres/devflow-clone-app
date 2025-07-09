2b66258aded19874d87d37f26cbd9cfe7683c8f5
Janriz Libres <libres.janriz@gmail.com>
2025-07-10 01:01:23 +0800

    modify ask a question skeletons

--- app/(root)/ask-question/loading.tsx
+++ app/(root)/ask-question/loading.tsx
@@ -2,12 +2,24 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
-    <section className="flex flex-col gap-10">
-      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+    <section>
+      <h1 className="mb-9 h1-bold text-dark100_light900">Ask a question</h1>
 
-      <Skeleton className="h-14 w-full" />
-      <Skeleton className="h-98 w-full" />
-      <Skeleton className="h-9 max-w-33" />
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-4 w-23" />
+          <Skeleton className="h-14 w-full" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+
+        <div>
+          <Skeleton className="mb-2 h-4 w-61" />
+          <Skeleton className="mb-4.5 h-98 w-full" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+      </div>
+
+      <Skeleton className="mt-26 ml-auto h-9 w-33" />
     </section>
   );
 };
