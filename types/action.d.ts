5b9f9ea9b4e113a5e658bc00e2af263c46fb3300
Janriz Libres <libres.janriz@gmail.com>
2025-07-13 08:44:19 +0800

    add update user action

--- types/action.d.ts
+++ types/action.d.ts
@@ -104,3 +104,11 @@ interface GlobalSearchParams {
   query: string;
   type: string | null;
 }
+
+interface UpdateUserParams {
+  name?: string;
+  username?: string;
+  email?: string;
+  image?: string;
+  password?: string;
+}
